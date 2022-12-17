6835cc86a15d6027a355293259a0cd57a4f7f7d1
Nerto Kageh <kagehnerto@yahoo.com>
2022-12-16 23:36:57 -0600

    Initial Commit

---
+++ Dice Game/Dice_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dice_GameApp.swift
+//  Dice Game
+//
+//  Created by Induran Aderinto  on 12/16/22.
+//
+
+import SwiftUI
+
+@main
+struct Dice_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
